97d3193476cd131b7ac9ae7dd6882e929699b642
Mukesh <mukesh.easternts@gmail.com>
2023-05-19 12:56:59 +0530

    Added types for package

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+declare module 'ets-fingerprintjs' {
+    export const uid: number;
+    export const fp_browser : string;
+    export const fp_flash: string;
+    export const fp_canvas: string;
+    export const fp_connection: string;
+    export const fp_cookie: string;
+    export const fp_display: string;
+    export const fp_fontsmoothing : string;
+    export const fp_fonts: string;
+    export const fp_formfields: string;
+    export const fp_java: string;
+    export const fp_language: string;
+    export const fp_silverlight: string;
+    export const fp_os: string;
+    export const fp_timezone: string;
+    export const fp_touch: string;
+    export const fp_plugins: string;
+    export const fp_useragent: string;
+    export const fp_truebrowser: string;
+}
